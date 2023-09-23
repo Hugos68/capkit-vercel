81a487cd505210450021db9ced9250f5db29114a
Hugos68 <hugokorteapple@gmail.com>
2023-09-23 10:36:49 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { Device, type DeviceInfo } from '@capacitor/device';
+    
+    let deviceInfo: DeviceInfo | null = null;
+
+    async function updateDeviceInfo() {
+        deviceInfo = await Device.getInfo();
+    }
+</script>
+
+<button on:click={updateDeviceInfo}>Get My Device Info Using Capacitor</button>
+
+{#if deviceInfo}
+    <p>Info: {JSON.stringify(deviceInfo)}</p>
+{/if}
